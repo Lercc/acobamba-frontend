0695a39233afef6d73f3b91605d0292fbf3996b8
lercc <lercc.en@gmail.com>
2021-07-13 16:47:51 -0500

    fix litle things

--- src/App.vue
+++ src/App.vue
@@ -4,34 +4,3 @@
     <router-view/>
   </div>
 </template>
-<script>
-import { mapMutations, mapState } from 'vuex';
-export default {
-  created() {
-    if (localStorage.getItem("token") && JSON.parse(localStorage.getItem("usuario"))) {
-
-        this.setTokenStoreData(localStorage.getItem("token"))
-        this.setUserStoreData( JSON.parse( localStorage.getItem("usuario") ) )
-
-        //redireccionar
-        if (localStorage.getItem('detalleClienteId')) this.$router.push({ name: 'detalles-aplicante', params: { clientId: localStorage.getItem('detalleClienteId')} })
-        else if(this.data.rol == 'employee') this.$router.push({ name : 'gestion-de-aplicantes' })
-        else if(this.data.rol == 'admin') this.$router.push({ name : 'inicio' })
-        
-    } else if (JSON.parse(localStorage.getItem("cliApp"))) {
-
-      this.setTokenCliAppStoreData(JSON.parse(localStorage.getItem("cliApp")))
-      this.$router.push({ name : 'registro-voucher'})
-    }
-  },
-  computed: {
-    ...mapState('user',['data']),
-  },
-  methods: {
-    ...mapMutations('token',['setTokenStoreData']),
-    ...mapMutations('user',['setUserStoreData']),
-
-    ...mapMutations('clientAplication',['setTokenCliAppStoreData']),
-  }
-}
-</script>
